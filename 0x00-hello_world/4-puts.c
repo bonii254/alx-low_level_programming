a8b4590837b6c0682f382776381a3cf15a3f0b74
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-16 17:30:19 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#visual <stdio.h>
+#include <stdio.h>
 /*
- * main -Entry
- * Return -bAlways 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle\n");
+	puts("\" Programming is like building a multilingual puzzle");
 	return (0);
 }
