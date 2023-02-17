9d732aa1ec5a768482d131b6184d101bca5e6e10
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-16 18:16:13 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
  * main - Entry
+ * Description: 'hello world'
  * Return: Always 0
  */
 int main(void)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Main - prints "with proper grammar, but the outcome is a piece of art", followed by new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
