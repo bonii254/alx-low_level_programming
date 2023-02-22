1fbe8fc01a349788b8c352f9537ba45420e2f411
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-22 06:49:53 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *_islower - check for lowercase
+  *Return: 1 if lowercase 0 if uppercase
+  *@c: the value to be checked
+  */
+
+int _islower(int c)
+{
+	if (_islower(c))
+		return (1);
+	return (0);
+}
