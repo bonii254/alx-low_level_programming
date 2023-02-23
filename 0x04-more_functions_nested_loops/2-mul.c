0ae375dd9577cd5a610b01245ad2f02bf2b36c9c
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-23 10:43:08 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *mul - multiplies two digit
+  *@a: first digit
+  *@b: second variable
+  *Return: successful 0
+  */
+
+int mul(int a, int b)
+{
+	int mul;
+	
+	mul = a * b;
+
+	return (mul);
+}
