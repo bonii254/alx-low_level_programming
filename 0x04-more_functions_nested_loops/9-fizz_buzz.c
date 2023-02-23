61317fdf200ce1aab24bc7785cc696bcd8fd5fc6
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-23 13:50:47 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 
 /**
   *main - print the number 1 to 1oo
-  * But for multiples of three print Fizz instead of the number
+  *But for multiples of three print Fizz instead of the number
   *for the multiples of five print Buzz
   *For numbers which are multiples of both three and five print FizzBuzz
   *Return: Always 0
@@ -19,7 +19,7 @@ int main(void)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0 && i % 3 != 0 )
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
 		}
