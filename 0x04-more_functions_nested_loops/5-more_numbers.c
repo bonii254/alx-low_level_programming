5edaad155747daba986257b9e2d373e8594246ed
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-23 16:03:38 -0800

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,13 +15,16 @@ void more_numbers(void)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			if (b >= 10)
+			if (b < 10)
 			{
-				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
+				_putchar(b + '0');
+			}
+			else
+			{
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
 			}
 		}
-
 		_putchar('\n');
 	}
 }
