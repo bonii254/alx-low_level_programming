bfc0b46f1ba07e3da7ced5a7aa6c700d5d34da2c
bonface murangiri <bonnyrangi95@gmail.com>
2023-02-24 12:34:24 -0800

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - causes an infinate loop
+  *Return: Always o
+  */
+
+int main(void)
+{
+	int i;
+
+	printf("infinate loop incoming :\n");
+	i = 0;
+	/*while(i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+	printf("infinite loop avoided! \\o/\n");
+	return (0);
+}
