3d2fc51c6b46f46f87f791716b4b95699cdb2c61
bonface murangiri <bonnyrangi95@gmail.com>
2023-03-21 08:33:30 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+  * struct dog - dog structure.
+  * @name: dogs name.
+  * @age: dogs age.
+  * @owner: dogs owner name.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
