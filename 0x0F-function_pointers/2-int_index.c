f0c6919fc3fdfafb114e2dd200a43a23be8a0e07
bonface murangiri <bonnyrangi95@gmail.com>
2023-04-19 09:24:36 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, j;
 
-	if (array == NULL && size <= 0 && cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	while (i < size)
