77bdff62d2d03e1870963cf033fa9e1e64151dad
bonface murangiri <bonnyrangi95@gmail.com>
2023-04-28 00:00:49 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * beat_hare - pre_main function.
+  */
+
+void beat_hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
