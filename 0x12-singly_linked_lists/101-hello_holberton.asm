a40d1579017f1a16e1b21433da671363a2bb0369
bonface murangiri <bonnyrangi95@gmail.com>
2023-04-28 02:49:55 -0700

    Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+SECTION .data
+msg: db "Hello, Holberton", 10, 0;
+
+SECTION .text
+extern printf
+global main
+main:
+	push ebp
+	move ebp, esp
+
+	push msg
+	call printf
+
+	mov esp, ebp
+	pop ebp
+	ret
