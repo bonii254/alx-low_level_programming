c3d4ed2a32a37a7fcaa17c3d26222bc619ce9c4a
bonface murangiri <bonnyrangi95@gmail.com>
2023-07-12 06:37:30 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (text_content != NULL`)
+	if (text_content != NULL)
 	{
 		while (text_content[i])
 			i++;
