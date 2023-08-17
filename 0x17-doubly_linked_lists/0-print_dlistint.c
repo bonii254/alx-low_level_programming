d8d02c98181047986374dfd099bfd3eebf2402bb
bonface murangiri <bonnyrangi95@gmail.com>
2023-08-17 15:22:56 +0000

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,15 +10,16 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num = 0;
-
-	if (h == NULL)
+	const dlistint_t *temp = h;
+	
+	if (!h)
 		return (0);
 
-	while (h != NULL)
+	while (temp != NULL)
 	{
 		num++;
-		printf("%d \n", h->n);
-		h = h->next;
+		printf("%d \n", temp->n);
+		temp = temp->next;
 	}
 	return (num);
 }
