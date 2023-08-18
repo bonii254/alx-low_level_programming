20499e4293d483453447bf418ad2d4a2291fcbdf
bonface murangiri <bonnyrangi95@gmail.com>
2023-08-18 12:57:59 +0000

    git add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
-	if (!new_node || !n)
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
