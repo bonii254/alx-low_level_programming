d4bb1ef2caf43f8209dc69bf29220c4e4257f2d0
bonface murangiri <bonnyrangi95@gmail.com>
2023-09-08 12:49:35 +0000

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - creates a hash table.
+  * @size: size of the hash table.
+  *
+  * Return: pointer to the newly created hash table. or
+  * null on fail.
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t * hashtable;
+	unsigned long int i;
+
+	hashtable = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (!hashtable)
+		return (NULL);
+	hashtable->size = size;
+	hashtable->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
+	return (hashtable);
+}
