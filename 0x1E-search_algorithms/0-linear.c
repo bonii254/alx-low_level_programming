285264d31f9233f17dd7f341626d5734693f05c3
bonface murangiri <bonnyrangi95@gmail.com>
2023-12-13 07:16:05 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,13 +15,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d] \n", i, *array);
-		if (*array == value)
+		if (*(array) == value)
 			return (i);
 		array++;
 	}
